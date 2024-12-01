dc38c4e09269dbc24a880ad242d95e26d8dfdf2e
SebastianFigueroaVenegas <carlos.figueroa43@inacapmail.cl>
2024-12-01 13:14:10 -0300

    Octavo commit

--- principal/forms.py
+++ principal/forms.py
@@ -3,13 +3,20 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 from .models import Cliente
+from django.db import models
 
 
 
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    is_staff = forms.BooleanField(required=False, label='¿Es staff?')
+    is_staff = forms.BooleanField(required=False, label="¿Es Recepcionista?")
+
+    is_staff = models.BooleanField(
+        default=False,  # Valor predeterminado
+    help_text='Designates whether the user can log into the admin site.',
+    )
+
 
     class Meta:
         model = User
